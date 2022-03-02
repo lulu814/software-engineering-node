770085dd416cb9526ed0003f290532c88fe455e7
Lu Liu <luliu@Lus-MacBook-Pro.local>
2022-03-02 11:39:18 -0500

    update DB credential string

--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 // mongoose.connect('mongodb://localhost:27017/tuit-db');
-mongoose.connect('mongodb+srv://lilaliu:CS5500lila@cluster0.i1c3p.mongodb.net/tuit-db?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.i1c3p.mongodb.net/tuit-db?retryWrites=true&w=majority');
 import bodyParser from "body-parser";
 import UserController from './controllers/UserController';
 import TuitController from "./controllers/TuitController";
@@ -28,6 +28,8 @@ import MessageController from "./controllers/MessageController";
 
 const app = express();
 app.use(bodyParser.json())
+const dotenv = require("dotenv")
+dotenv.config()
 
 // create RESTful Web service API
 const userController = UserController.getInstance(app);
