88e274e90483325990d1678dc983e6f2832fc018
Lu Liu <luliu@Lus-MacBook-Pro.local>
2022-03-27 14:37:30 -0400

    add dislike to model and schema

--- mongoose/dislikes/DislikeSchema.ts
+++ mongoose/dislikes/DislikeSchema.ts
@@ -1,16 +1,16 @@
 /**
- * @file Implements mongoose schema for likes
+ * @file Implements mongoose schema for dislikes
  */
 import mongoose, {Schema} from "mongoose";
-import Like from "../../models/likes/Like";
+import Dislike from "../../models/likes/Dislike";
 
 /**
- * @typedef Like Represents the like relation between a user and a tuit
- * @property {ObjectId} tuit The id of the tuit like by user
- * @property {ObjectId} likedBy The id of the user
+ * @typedef Dislike Represents the dislike relation between a user and a tuit
+ * @property {ObjectId} tuit The id of the tuit disliked by user
+ * @property {ObjectId} dislikedBy The id of the user
  */
-const LikeSchema = new mongoose.Schema<Like>({
+const DislikeSchema = new mongoose.Schema<Dislike>({
     tuit: {type: Schema.Types.ObjectId, ref: "TuitModel"},
-    likedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
-}, {collection: "likes"});
-export default LikeSchema;
+    dislikedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
+}, {collection: "dislikes"});
+export default DislikeSchema;
